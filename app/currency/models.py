c03eed255f9e48341c91d1bc115fd6c31f048e60
Danyil <daniilkiselev443@gmail.com>
2022-08-17 07:47:20 -0400

    hw7

--- app/currency/models.py
+++ app/currency/models.py
@@ -3,6 +3,15 @@ from django.db import models # noqa
 # Create your models here.
 
 
+class Rate(models.Model):
+    base_currency_type = models.CharField(max_length=3)
+    currency_type = models.CharField(max_length=3)
+    sale = models.DecimalField(max_digits=10, decimal_places=4)
+    buy = models.DecimalField(max_digits=10, decimal_places=4)
+    source = models.CharField(max_length=64)
+    created = models.DateTimeField(auto_now_add=True)
+
+
 class ContactUs(models.Model):
     email_from = models.EmailField()
     email_to = models.EmailField()
